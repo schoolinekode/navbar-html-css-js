70adb84f023935e6cc3d8c571097f148b14f62eb
Alpha DIOP <alphakhoss@gmail.com>
2024-06-05 16:35:33 +0000

    navbar pour les null #0.0.1

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
+// Sélectionne l'élément de navigation et le bouton du menu par leurs ID
 const navToggle = document.getElementById("navigation");
 const menu = document.getElementById("menu");
 
+// Ajoute un écouteur d'événements pour le clic sur le bouton du menu
 menu.addEventListener('click', function () {
-    navToggle.classList.toggle('active');
-})
+  // Bascule la classe 'active' sur l'élément de navigation pour afficher ou masquer le menu
+  navToggle.classList.toggle('active');
+});
